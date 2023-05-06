dbc78e67f552d324d31117048a512ff7cf8d4631
tarrim80 <118617900+tarrim80@users.noreply.github.com>
2023-05-07 02:04:33 +0600

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework import routers
-
 from api import views
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+        path('sentry-debug/', trigger_error), # Новый маршрут.
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]

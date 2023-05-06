f2d1e3d560761561c0543e9a9019e915ddbde1f3
tarrim80 <tarrim80@yandex.ru>
2023-05-06 19:59:01 +0000

    Update from server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,15 @@
+import sentry_sdk
 from pathlib import Path
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.27.241', '127.0.0.1', 'localhost', 'tarrim80.hopto.org']
 
 
 # Application definition
@@ -103,7 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -113,3 +116,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
+
+
+# senry sdk
+sentry_sdk.init(
+    dsn = '',
+    integrations = [
+        DjangoIntegration(),
+    ],
+    traces_sample_rate = 1.0,
+    send_default_pii = True
+)

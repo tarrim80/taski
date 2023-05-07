77ebd62cbc744d47235ac431e46e52dbaf49bfcd
tarrim80 <tarrim80@yandex.ru>
2023-05-07 03:22:49 +0000

    Revert "Update urls.py"
    This reverts commit dbc78e67f552d324d31117048a512ff7cf8d4631.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
-from api import views
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
+from api import views
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
-        path('sentry-debug/', trigger_error), # Новый маршрут.
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
